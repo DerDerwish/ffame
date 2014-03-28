41e148774f8f1f66fccaab92f8f849a27f1605c4
DerDerwish <derderwish@metameute.de>
2014-03-28 18:23:10 +0100

    initial rails angelegt

---
+++ ffame/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ffame::Application.config.secret_key_base = '09ca62664903adf9f1b19e9e9b00ad26c9a9e0a439f702c15cce9a0f565a38ad6ca0b51090a99062ddaa9221018fd833cf5ecdaec48f1213f1988884245474a9'
